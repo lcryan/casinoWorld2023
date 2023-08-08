7d1fc12be778606af823b17693b9ef1a74f294b0
LuciaRyan <lcryan06@gmail.com>
2023-08-08 20:08:12 +0200

    deleted shuffle method to make a more straight forward method

--- src/main/java/org/example/BlackJack/Deck.java
+++ src/main/java/org/example/BlackJack/Deck.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class Deck {
 
-    protected Stack<Card> cards;
+    private Stack<Card> cards = new Stack<>();
 
 
     public Deck() {
@@ -13,20 +13,14 @@ public class Deck {
     }
 
     public void shuffle() {
-        Random setOfCards = new Random(); // creating a new instance of the Random class - which is used to generate random numbers - here a random set of cards 0 - each time nextInt is being called - it returns a random integer. //
-        for (int i = 0; i < setOfCards.nextInt(); i++) { // for-loop : iterating through the cards and shuffling them  // // an idea would be to maybe iterate a certain amount of times ??? //
-            int shuffledCards = setOfCards.nextInt(cards.size()); // this line generates a random index within the range of the current number of cards in the deck (here: cards.size()). This index will be used to select a random card to swap with the card at the current index ('i') during shuffling //
-            Card hold = cards.get(i); //this line temporarily stores the card at the current index (i) in a variable named "hold". This is done  to make the swapping process possible;
-            cards.set(i, cards.get(shuffledCards)); // this line swaps the card at index 'i' with the card at index 'shuffledCards'; ergo:  It moves a randomly selected card to the current position in the iteration//
-            cards.set(shuffledCards, hold); // this line then places the card that was originally at index 'i' (stored in the 'hold' variable) into the position of the randomly selected card index (index "shuffledCards"). This completes the swapping process. //
         }
-    }
+
 
     public Card getNextCard() {
-        return cards.pop(); // the pop method pon cards removes and returns the top element from the stack //
+        return cards.pop(); // the pop method on cards removes and returns the top element from the stack //
     }
 
     public boolean isEmpty() {
-        return cards.isEmpty(); // will be true / false depending on each case //
+        return true; // why is this true here ??? //
     }
 }
