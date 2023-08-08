447b7b47698b219073b775c901882c31336f0e59
LuciaRyan <lcryan06@gmail.com>
2023-08-08 20:29:31 +0200

    added card numbers and names as list of ints and Strings - further inserted values as in assignment to each new CardSuit

--- src/main/java/org/example/BlackJack/Deck.java
+++ src/main/java/org/example/BlackJack/Deck.java
@@ -9,11 +9,22 @@ public class Deck {
 
 
     public Deck() {
-//TODO : do I have to add something here ?!? TBD. //
+        int[] numbers = new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+        String[] names = new String[]{"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"};
+        CardSuit[] suits = new CardSuit[]{
+                new CardSuit("diamonds", '\u2666', "red"),
+                new CardSuit("spades", '\u2660', "black"),
+                new CardSuit("hearts", '\u2665', "red"),
+                new CardSuit("clubs", '\u2663', "black")
+        };
+
     }
 
+    ;
+
+
     public void shuffle() {
-        }
+    }
 
 
     public Card getNextCard() {
