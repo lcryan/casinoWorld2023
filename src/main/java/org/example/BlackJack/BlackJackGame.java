c56f5cfaed70b33da6135cc1db561667d8d8e993
LuciaRyan <lcryan06@gmail.com>
2023-08-08 20:07:27 +0200

    deleted shuffle method to make a more straight forward method

--- src/main/java/org/example/BlackJack/BlackJackGame.java
+++ src/main/java/org/example/BlackJack/BlackJackGame.java
@@ -1,24 +1,23 @@
 package org.example.BlackJack;
-
-import java.util.Random;
 import java.util.Scanner;
 
 public class BlackJackGame {
     private Scanner inputScanner;
     private boolean gameIsRunning;
     private Deck deck;
-    private Player playerA;
+    private Player player;
     private Dealer dealer;
 
     public BlackJackGame(Scanner inputScanner) {
         this.inputScanner = inputScanner;
         this.deck = new Deck();
+        gameIsRunning = true;
+        player = new Player();
+        dealer = new Dealer();
     }
 
     public void playJackGame() {
-        gameIsRunning = true;
-        playerA = new Player();
-        dealer = new Dealer();
+
 // shuffle //
         //
     }
